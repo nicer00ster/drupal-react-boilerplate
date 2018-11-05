f48732728c91d2789a4a06b136e686072ff7fe62
Alex Busch <abusch1013@gmail.com>
2018-11-05 14:23:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        It's working!!!
+      </div>
+    )
+  }
+}
+
+export default App;
